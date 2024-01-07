411af52b1cdc50202f2be64fcfc875a3fb49da7e
Snmngc <snmngc@gmail.com>
2024-01-07 19:12:26 +0300

    HashMap

--- CarFunc.rs
+++ CarFunc.rs
@@ -7,19 +7,36 @@ enum Transmission { Manual, SemiAuto, Automatic }
 #[derive(PartialEq, Debug)]
 enum Age { New, Used }
 
+
 fn car_quality (miles: u32) -> (Age, u32) {
 
-    let quality = (Age::New, miles);
 
-    quality
+    if miles > 0 {
+        return (Age::Used, miles);
+    }
+    
+    (Age::New, miles)
 }
 
+fn car_factory(order: i32, miles: u32) -> Car {
+    let colors = ["Blue", "Green", "Red", "Silver"];
 
-fn car_factory(color: String, motor: Transmission, roof: bool, miles: u32) -> Car {
-
+    let mut color = order as usize;
+    if color > 4 {        
+        color = color - 4;
+    }
+        
+    let mut motor = Transmission::Manual;
+    let mut roof = true;
+    if order % 3 == 0 {          // 3, 6, 9
+        motor = Transmission::Automatic;
+    } else if order % 2 == 0 {   // 2, 4, 8, 10
+        motor = Transmission::SemiAuto;
+        roof = false;
+    }                            // 1, 5, 7, 11
 
     Car {
-        color: color,
+        color: String::from(colors[(color-1) as usize]),
         motor: motor,
         roof: roof,
         age: car_quality(miles)
@@ -27,19 +44,42 @@ fn car_factory(color: String, motor: Transmission, roof: bool, miles: u32) -> Ca
 }
 
 fn main() {
-
-    let colors = ["Blue", "Green", "Red", "Silver"];
+    use std::collections::HashMap;
+    let mut orders: HashMap<i32, Car> = HashMap::new(); 
+    let mut order = 1;
     let mut car: Car;
-    let mut engine = Transmission::Manual;
 
-    car = car_factory(String::from(colors[0]), engine, true, 0);
-    println!("Car order 1: {:?}, Hard top = {}, {:?}, {}, {} miles", car.age.0, car.roof, car.motor, car.color, car.age.1);
-    
-    engine = Transmission::SemiAuto;
-    car = car_factory(String::from(colors[1]), engine, false, 100);
-    println!("Car order 2: {:?}, Hard top = {}, {:?}, {}, {} miles", car.age.0, car.roof, car.motor, car.color, car.age.1);
+    car = car_factory(order, 1000);
+    orders.insert(order, car);
+    println!("Car order {}: {:?}", order, orders.get(&order));
 
-    engine = Transmission::Automatic;
-    car = car_factory(String::from(colors[2]), engine, true, 200);
-    println!("Car order 3: {:?}, Hard top = {}, {:?}, {}, {} miles", car.age.0, car.roof, car.motor, car.color, car.age.1);
+    // Car order #2: Used, Convertible
+    order = order + 1;
+    car = car_factory(order, 2000);
+    orders.insert(order, car);
+    println!("Car order {}: {:?}", order, orders.get(&order));
+
+    // Car order #3: New, Hard top
+    order = order + 1;
+    car = car_factory(order, 0);
+    orders.insert(order, car);
+    println!("Car order {}: {:?}", order, orders.get(&order));
+
+    // Car order #4: New, Convertible
+    order = order + 1;
+    car = car_factory(order, 0);
+    orders.insert(order, car);
+    println!("Car order {}: {:?}", order, orders.get(&order));
+
+    // Car order #5: Used, Hard top
+    order = order + 1;
+    car = car_factory(order, 3000);
+    orders.insert(order, car);
+    println!("Car order {}: {:?}", order, orders.get(&order));
+
+    // Car order #6: Used, Hard top
+    order = order + 1;
+    car = car_factory(order, 4000);
+    orders.insert(order, car);
+    println!("Car order {}: {:?}", order, orders.get(&order));
 }
